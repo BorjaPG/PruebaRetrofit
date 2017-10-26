91eed40e5ffc70bc118bc99974f5e7b40f3ed377
Borja <Borjapg10@gmail.com>
2017-10-26 23:46:45 +0200

    Modificación de comentarios.

--- app/src/main/java/com/bp/pruebaretrofit/Api/BlogApi.java
+++ app/src/main/java/com/bp/pruebaretrofit/Api/BlogApi.java
@@ -1,8 +1,14 @@
 package com.bp.pruebaretrofit.Api;
 
 import com.bp.pruebaretrofit.Model.RecentPosts;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 /**
  * Las interfaces contienen todas las peticiones soportadas por la API.
@@ -10,14 +16,33 @@ import retrofit2.http.GET;
 
 public interface BlogApi {
 
-    /* En este caso sólo contiene una petición. Sólo tiene un método GET. */
-    @GET("/?json=get_recent_posts") //Path API correspondiente a la petición.
+    /* A la URL base "http://www.tutos-android.com/" se le concatena "/?json=get_recent_posts"
+    * y de esta forma la petición obtiene en este caso los post más recientes. */
+    @GET("?json=get_recent_posts") //Dirección donde se hará la petición.
+
     /* getRecentPosts devuelve como resultado Call<RecentPosts>, que se corresponde con un
     * objeto que agrupa la petición enviada.*/
     Call<RecentPosts> getRecentPosts();
 
-    /* Existen otros métodos básicos de las API rest:
-    *   @POST
-    *   @DELETE
-    *   @UPDATE */
+
+
+    /* Usando @Query. Cuando se llame a obtenerPosts se concatenará a la URL lo que le pasemos
+    * como parámetro. Se utiliza cuando los parámetros son dinámicos. */
+    //@GET("/")
+    //Call<RecentPosts> obtenerPosts(@Query("get_recent_posts") String ultimosPosts);
+
+
+    /* Usando @QueryMap. Se usa cuando se desea agregar muchos parámetros al método.
+     Se pasa un Map con las claves y los valores.*/
+    //@GET("/?json=get_recent_posts")
+    //Call<RecentPosts> obtenerPostSelectivos(@QueryMap Map<String, String> filtros);
+
+    //Se construye un mapa con las claves y valores.
+    //Map<String, String> filtros = new HashMap<String,String>;
+    //filtros.put("activo", "1");
+    //filtros.put("display","full");
+    //adaptador.obtenerPostSelectivos(filtros);
+
+    //Se obtendría la dirección: /get_recent_posts?activo=1&display=full
+
 }
